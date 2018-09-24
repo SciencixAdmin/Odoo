5e2155148bb76e8a288f657432a27c9efd295cbc
Carl Molnar <carl.molnar@sciencix.com>
2018-09-24 08:54:02 -0500

    Added test theme

---
+++ theme_tutorial/__manifest__.py
@@ -0,0 +1,19 @@
+{
+  'name':'Tutorial theme',
+  'description': 'A description for your theme.',
+  'version':'1.0',
+  'author':'Your name',
+
+  'data': [
+    'views/layout2.xml',
+    'views/pages.xml',
+    'views/assets.xml',
+    'views/snippets.xml',
+    'views/options.xml'
+  ],
+  'category': 'Theme/Creative',
+  'depends': ['website'],
+  'license': 'OEEL-1',
+  'installable': True,
+  'auto_install': False,
+}
