a7c0e93b153487fb417936636d57746aab0ce6f3
Carl Molnar <carl.molnar@sciencix.com>
2018-09-24 09:42:27 -0500

    Testing module

---
+++ theme_sciencix - Copy/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- encoding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Sciencix: Test',
+    'version': '1.0',
+    'category': 'Hidden',
+    'sequence': 50,
+    'summary': """Change Header Color""",
+    'depends': ['web_enterprise'],
+    'description': """
+* Change Header Color: #32CD32
+
+""",
+    'data': [
+        'views/webclient_templates.xml',
+    ],
+    'license': 'OEEL-1',
+}
