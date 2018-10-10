b067541d82cb0a26f431d7628342fda3f66670d5
Jigar A <jam@odoo.com>
2018-10-10 10:56:27 -0700

    [MERGE] merge product files

--- sciencix_sale/models/product_template.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-
-from odoo import api, fields, models, tools
-
-class ProductTemplate(models.Model):
-    _inherit = "product.template"
-
-    website_notes = fields.Text('Website Notes', translate=True, help='Notes that will appear on the website product page')
