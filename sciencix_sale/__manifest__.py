fa0b27027b69004c2391d297919f026b5dcf9c24
cic-odoo <cic@odoo.com>
2019-01-31 10:05:53 -0800

    [ADD] Changes to init/manifest to resolve conflicts

--- sciencix_sale/__manifest__.py
+++ sciencix_sale/__manifest__.py
@@ -55,16 +55,13 @@ Various additions to the SO/PROFORMA-INVOICE/INVOICE reports.
         'report/report_delivery_slip.xml',
         'report/report_sale_order.xml',
         'report/report_account_invoice.xml',
-<<<<<<< HEAD
+        'report/reprot_product_label.xml',
         #Report mod dev3
         'report/inherited_sale_report_templates.xml',
         'report/inherited_invoice_report_templates.xml',
-=======
-        'report/reprot_product_label.xml',
         #View refer to reprot
         'views/stock_picking.xml',
 
->>>>>>> c911e52e1f401b91ebe5028e93bba0b1313e799d
     ],
     'demo': [],
     'qweb': [],
--- sciencix_sale/models/__init__.py
+++ sciencix_sale/models/__init__.py
@@ -5,8 +5,5 @@ from . import partner
 from . import sale_order
 from . import account_invoice
 from . import stock_move
-<<<<<<< HEAD
-from . import inherited_sale_order, inherited_account_invoice
-=======
 from . import stock_picking
->>>>>>> c911e52e1f401b91ebe5028e93bba0b1313e799d
+from . import inherited_sale_order, inherited_account_invoice
