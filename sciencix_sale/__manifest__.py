e0d57b3e699bf028c66a73cdd41106fa8d4d9d58
cic-odoo <cic@odoo.com>
2019-02-25 16:55:21 -0800

    [ADD][1923767] Added new branch for dev. Also added new requirements from UAT feedback

--- sciencix_sale/__manifest__.py
+++ sciencix_sale/__manifest__.py
@@ -32,11 +32,16 @@ Link this Alias to the Sale Order line. For Product "ABC123", I have 2 alias def
 When I create the SO for CustomerA and select product "ABC123", the system will display me the alias that I can choose for this product for the customer on the SO.
 The alias that is selected in this SO should be printed in the SO report (additional column"Product" before the description column)
 When I create the Invoice and Delivery Order, this Alias should pass on to the invoice and Delivery Order Form with the new added column.
-Invoice Report and Delivery Slip report to have Product Column before Description
+Invoice Report and Delivery y
+Slip report to have Product Column before Description
 In the model "Alias" The product name is at the variant level.
+
+Sales Development Part 3
+========================
+Various additions to the SO/PROFORMA-INVOICE/INVOICE reports.
     """,
     'category': 'Custom Development',
-    'depends': ['sale_management', 'delivery', 'website_sale', 'sale_stock'],
+    'depends': ['sale_management', 'delivery', 'website_sale', 'sale_stock','account'],
     'data': [
         # 'security/sciencix_sale.xml',
         'security/ir.model.access.csv',
@@ -52,6 +57,7 @@ In the model "Alias" The product name is at the variant level.
         'report/report_delivery_slip.xml',
         'report/report_sale_order.xml',
         'report/report_account_invoice.xml',
+
     ],
     'demo': [],
     'qweb': [],
--- sciencix_sale/models/account_invoice.py
+++ sciencix_sale/models/account_invoice.py
@@ -3,9 +3,12 @@ from odoo import models, fields, api, _
 from odoo.exceptions import ValidationError
 
 class AccountInvoiceLine(models.Model):
-    _inherit = 'account.invoice.line'
+    _inherit = ['account.invoice.line']
 
     product_alias_id = fields.Many2one('product.alias', ondelete='set null', string='Product Alias', readonly=False)
+
+    inv_hs_code = fields.Char(compute="_compute_hs_origin", string="Schedule B Number")
+    inv_origin_id = fields.Many2one('res.country',compute="_compute_hs_origin",string="Country of Origin")
 
     @api.model
     def create(self, vals):
@@ -29,3 +32,43 @@ class AccountInvoiceLine(models.Model):
             else:
                 self.product_alias_id = False
 
+    @api.multi
+    @api.depends('product_id','sale_line_ids')
+    def _compute_hs_origin(self):
+        for line in self:
+            if line.product_id:
+                cur_code = line.product_id.hs_code
+                cur_country = line.product_id.country_origin
+
+                line.inv_hs_code = cur_code
+                line.inv_origin_id = cur_country
+
+class AccountInvoice(models.Model):
+    _inherit = ['account.invoice']
+
+    inv_picking_policy = fields.Selection([
+        ('direct', 'Deliver each product when available'),
+        ('one', 'Deliver all products at once')],
+        compute="_compute_sale_fields", string="Shipping Policy", store=True)
+
+    sale_order_id = fields.Many2one('sale.order',compute="_compute_origin_sale", string="Original Sale Order",store=True)
+    inv_add_notes = fields.Text(related='sale_order_id.add_notes', string="Additional Notes", store=True)
+    inv_cust_ref = fields.Char(related='sale_order_id.cust_ref', string="Customer Ref", store=True)
+
+    @api.multi
+    @api.depends('invoice_line_ids','invoice_line_ids.sale_line_ids','invoice_line_ids.sale_line_ids.order_id')
+    def _compute_origin_sale(self):
+        for account in self:
+            if account.invoice_line_ids:
+                if account.invoice_line_ids[0].sale_line_ids:
+                    cur_order = account.invoice_line_ids[0].sale_line_ids[0].order_id
+                    account.sale_order_id = cur_order
+
+    @api.multi
+    @api.depends('invoice_line_ids','invoice_line_ids.sale_line_ids','invoice_line_ids.sale_line_ids.order_id')
+    def _compute_sale_fields(self):
+        for account in self:
+            if account.invoice_line_ids:
+                if account.invoice_line_ids[0].sale_line_ids:
+                    cur_shipping = account.invoice_line_ids[0].sale_line_ids[0].order_id.picking_policy
+                    account.inv_picking_policy = cur_shipping
