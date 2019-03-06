035a0b3f8aed9c211d7a03d628c421ac4230506a
Jigar A. (Odoo) <jam@odoo.com>
2019-03-06 13:33:53 -0800

    [IMP] make seller_product_code stored for historical reason

--- sciencix_purchase/models/purchase.py
+++ sciencix_purchase/models/purchase.py
@@ -8,23 +8,20 @@ from odoo import api, fields, models
 class PurchaseOrderLine(models.Model):
 
     _inherit = 'purchase.order.line'
-
-
-    @api.multi
-    @api.depends('product_id', 'partner_id')
-    def _compute_seller_product_code(self):
-        for line in self:
-            seller_product_code = line.product_id.default_code if line.product_id else ''
-            if line.partner_id and line.product_id:
-                seller = line.product_id._select_seller(
-                    partner_id=line.partner_id,
-                    quantity=line.product_qty,
-                    date=line.order_id.date_order and line.order_id.date_order[:10],
-                    uom_id=line.product_uom)
-                if seller and seller.product_code:
-                    seller_product_code = seller.product_code
-            line.seller_product_code = seller_product_code
-
     
-    seller_product_code = fields.Char(compute='_compute_seller_product_code', string="Seller Product Code")
+    seller_product_code = fields.Char(string="Seller Product Code")
     
+    @api.onchange('product_qty', 'product_uom')
+    def _onchange_quantity(self):
+        super(PurchaseOrderLine, self)._onchange_quantity()
+        if not self.product_id:
+            return
+
+        seller = self.product_id._select_seller(
+            partner_id=self.partner_id,
+            quantity=self.product_qty,
+            date=self.order_id.date_order and self.order_id.date_order[:10],
+            uom_id=self.product_uom)
+
+        if seller:
+            self.seller_product_code = seller.product_code
