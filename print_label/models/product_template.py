7c3bdb7c5421a1326f7e0a6f35601fab656a1ae4
ehe <ehe@odoo.com>
2019-07-11 22:44:37 +0000

    [MIG] purchase reports

--- print_label/models/product_template.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields, api
-
-
-class ProductTemplate(models.Model):
-    _inherit = 'product.template'
-    
-    oem = fields.Char(string='OEM')
